03485724b8d88afb15474ed4b9731c309bc828e2
juanpabloprado <juan@juanpabloprado.com>
2016-09-23 21:13:18 -0500

    Initial commit

---
+++ src/test/java/com/juanpabloprado/learningspringboot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,18 @@
+package com.juanpabloprado.learningspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LearningSpringBootApplication.class)
+@WebAppConfiguration
+public class LearningSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
