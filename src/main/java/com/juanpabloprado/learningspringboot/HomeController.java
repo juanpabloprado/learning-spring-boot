ff4845f9ce10845379d679c3afee1b2502012eae
juanpabloprado <juan@juanpabloprado.com>
2016-09-25 02:11:42 -0500

    Add page navigation

--- src/main/java/com/juanpabloprado/learningspringboot/HomeController.java
+++ src/main/java/com/juanpabloprado/learningspringboot/HomeController.java
@@ -39,6 +39,12 @@ public class HomeController {
   public String index(Model model, Pageable pageable) {
     final Page<Image> page = imageService.findPage(pageable);
     model.addAttribute("page", page);
+    if(page.hasPrevious()) {
+      model.addAttribute("prev", pageable.previousOrFirst());
+    }
+    if(page.hasNext()) {
+      model.addAttribute("next", pageable.next());
+    }
     return "index";
   }
 
